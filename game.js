d36567f52b578bd59c19edc2557ad37ed0a98db6
Logan Webber <logan.webber@hotmail.com>
2020-11-29 16:10:36 +1300

    Completed Game Board in the DOM

--- game.js
+++ game.js
@@ -11,7 +11,11 @@ function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
+      dots[i].addEventListener('contextmenu', makeGreen)
+      dots[i].addEventListener('click', makeBlue)
+      dots[i].addEventListener('dblclick', hide)
+
+     
   }
 }
 
@@ -23,19 +27,33 @@ function makeGreen (evt) {
 
 // CREATE FUNCTION makeBlue HERE
 
+function makeBlue (evt) {
+    evt.target.classList.toggle('blue')
+    updateCounts()
+}
+
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+    evt.target.classList.toggle('invisible')
+    updateCounts()
+}
 
-function updateCounts () {
-  var totals = {
-    blue: 0,
-    green: 0,
-    invisible: 0
-  }
-  
-  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+function updateCounts() {
+    var totals = {
+        blue: 0,
+        green: 0,
+        invisible: 0
+    }
 
-  // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+    // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+     {
+        totals.blue = document.getElementsByClassName('blue').length;
+        totals.green = document.getElementsByClassName('green').length;
+        totals.invisible = document.getElementsByClassName('invisible').length;
+    }
+
+    // Once you've done the counting, this function will update the display
+    displayTotals(totals)
 }
 
 function displayTotals (totals) {
